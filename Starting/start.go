a0fdaf94afab372306e38a15c28ac8fd875c6c70
Stas <belakstas10@gmail.com>
2026-05-22 22:44:28 +0300

    Много

--- Starting/start.go
+++ Starting/start.go
@@ -10,8 +10,8 @@ func Start() {
 
 	fmt.Printf("Приветсвую вас в игре!!!\nДайте имя персонажу: ")
 	fmt.Scan(&name)
-	player := NewPlayer(name, 100, 10)
-	unit := NewUnit(20, 4)
+	player := NewPlayer(name)
+	unit := NewUnit()
 
 	fmt.Printf("Приветсвую %s\nВаш HP: %d\nВаш урон: %d", player.Name, player.HP, player.Damage)
 
@@ -23,7 +23,51 @@ func Start() {
 
 	fmt.Print("\033[H\033[2J")
 
-	Attack(unit, player)
-	Output(player, unit)
+	for {
+		fmt.Println("1: Информация | 2: Левл | 3: Exit")
+		var b int16
+		fmt.Scan(&b)
+		time.Sleep(time.Second * 2)
+		fmt.Print("\033[H\033[2J")
+
+		if b == 3 {
+			break
+		} else if b == 2 {
+			fmt.Println("Введите номер лвл (1-10): ")
+			var c int
+			fmt.Scan(&c)
+			if c <= 0 {
+				fmt.Println("Ошибка!!!")
+				continue
+			} else {
+				levl(unit, c)
+				var count int
+				for {
+					fmt.Println("1: Attack | 2: Exite")
+					fmt.Scan(&count)
+					fmt.Print("\033[H\033[2J")
+
+					if count == 1 {
+						Attack(unit, player)
+						if unit.HP <= 0 {
+							fmt.Print("\033[H\033[2J")
+							fmt.Printf("Вы прошли %d лвл\n", c)
+							continue
+						}
+						if player.HP <= 0 {
+							fmt.Println("Вы умерли весь ваш прогресс был снят")
+							unload_p(player)
+						}
+						continue
+					} else {
+						break
+					}
+				}
+			}
+		} else if b == 1 {
+			fmt.Printf("Имя: %s; HP: %d; Урон: %d\n", (*player).Name, (*player).HP, (*player).Damage)
+			continue
+		}
+	}
 
 }
